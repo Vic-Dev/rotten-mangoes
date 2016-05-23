6c4d7c38d63ab0eb4450f937845df17cb9fcbd86
Victoria Johns <vmichellej@gmail.com>
2016-05-23 16:45:13 -0400

    Use helper methods

--- rotten_mangoes/app/models/user.rb
+++ rotten_mangoes/app/models/user.rb
@@ -5,15 +5,19 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates :email,
-    presence: true
- 
+  presence: true
+  
   validates :firstname,
-    presence: true
- 
+  presence: true
+  
   validates :lastname,
-    presence: true
- 
+  presence: true
+  
   validates :password,
-   length: { in: 6..20 }, on: :create
+  length: { in: 6..20 }, on: :create
+
+  def full_name
+    "#{firstname} #{lastname}"
+  end
 
 end
