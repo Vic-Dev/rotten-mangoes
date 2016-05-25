c721422d531ee408f4f9e829b59b4f77dc624c74
Victoria Johns <vmichellej@gmail.com>
2016-05-25 13:45:33 -0400

    Add admin edit users

--- rotten_mangoes/app/controllers/admin/users_controller.rb
+++ rotten_mangoes/app/controllers/admin/users_controller.rb
@@ -6,8 +6,26 @@ class Admin::UsersController < ApplicationController
     @users = User.all.page(params[:page]).per(10)
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
     @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to admin_users_path
+    else
+      render :edit
+    end
   end
   
   private
@@ -17,6 +35,10 @@ class Admin::UsersController < ApplicationController
       flash[:error] = "You are not authorized to view that page."
       redirect_to root_path
     end
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation, :admin)
   end
 
 end
